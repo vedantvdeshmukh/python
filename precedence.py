e0ddeaf7539242c6ba73f6104c33ad9070462122
Vedant Deshmukh <vedantdeshmukh105@gmail.com>
2026-01-18 15:50:49 +0530

    uniry associative exponential

---
+++ precedence.py
@@ -0,0 +1,22 @@
+name = "Vedant"
+age = 22
+print(f"My name is {name} & age is {age}")
+condition = name == "Vedant" or name == "ram" and age < 18
+print("condition is ",condition)
+print(2**1**3) # exponentiation is right associative
+print( (2**1)**3 ) # exponentiation
+print( 2**(1**3) ) # exponentiation
+print(10/2*3) # left to right
+print( (10/2)*3 ) # division and multiplication
+print( 10/(2*3) ) # division and multiplication
+
+not True
+print("not True is ",not True)
+not False           
+print("not False is ",not False)
+
+not 0
+
+print("not 0 is ",not 0)
+not 5
+print("not 5 is ",not 5)
